e869a3e74d11f630d8efa77ad48adfe66a3644eb
ClintonTebogo999 <clienton30sep@gmail.com>
2025-08-24 22:09:08 +0200

    Add files via upload

---
+++ handbag/handbag/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace handbag
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Quantity = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.Navy;
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.ForeColor = System.Drawing.Color.White;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Items.AddRange(new object[] {
+            "Full Decorative Bag",
+            "Beaded bag",
+            "Needlepoint Design bag",
+            "fringed beaded bag",
+            "plain bag"});
+            this.listBox1.Location = new System.Drawing.Point(17, 82);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(222, 112);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(13, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "SELECT BAG";
+        
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label2.Location = new System.Drawing.Point(285, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Enter Quatity";
+            // 
+            // Quantity
+            // 
+            this.Quantity.BackColor = System.Drawing.Color.Navy;
+            this.Quantity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Quantity.ForeColor = System.Drawing.SystemColors.InactiveBorder;
+            this.Quantity.Location = new System.Drawing.Point(279, 82);
+            this.Quantity.Multiline = true;
+            this.Quantity.Name = "Quantity";
+            this.Quantity.Size = new System.Drawing.Size(116, 43);
+            this.Quantity.TabIndex = 3;
+            this.Quantity.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton1.Location = new System.Drawing.Point(435, 96);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(92, 20);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Overnight";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton2.Location = new System.Drawing.Point(435, 120);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(99, 20);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Three Day";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radioButton3.Location = new System.Drawing.Point(435, 143);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(89, 20);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Standard";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label3.Location = new System.Drawing.Point(431, 58);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(212, 20);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Select Shipping Options :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(212, 48);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "CHECKOUT ORDER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label4.Location = new System.Drawing.Point(12, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "BAG THEE BAG ";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 315);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(212, 48);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "CANCEL SELECTION";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(656, 513);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.Quantity);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Name = "Form1";
+            this.Text = "Form1";
+          
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Quantity;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ handbag/handbag/Form1.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace handbag
+{
+    public partial class Form1 : Form
+    {
+        public double price ,quantity;
+        public double shippingCharges ,shippingChargePerc;
+
+        public double calculateQuantity(double Price, double Quantity)
+        {
+            return quantity * Price;
+        }
+        public double calculateShippingCharges()
+        {
+
+            return 0;
+        }
+
+        
+        public Form1()
+        {
+            InitializeComponent();
+           
+        
+        }
+        
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            if( listBox1.SelectedIndex == 0)
+            {
+                price = 450;
+                MessageBox.Show($"price for { listBox1.SelectedItem.ToString()} is  R{price}");
+
+            }
+            else if (listBox1.SelectedIndex == 1)
+            {
+                price = 545;
+                MessageBox.Show($"price for { listBox1.SelectedItem.ToString()} is  R{price}");
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                price = 240;
+                MessageBox.Show($"price for { listBox1.SelectedItem.ToString()} is  R{price}");
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                price = 630;
+                MessageBox.Show($"price for { listBox1.SelectedItem.ToString()} is  R{price}");
+            }
+            else if (listBox1.SelectedIndex == 4)
+            {
+                price = 120;
+                MessageBox.Show($"price for { listBox1.SelectedItem.ToString()} is  R{price}");
+            }
+            
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            shippingCharges = 0.10;
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            shippingCharges = 0.07;
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            shippingCharges = 0.05;
+        }
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedIndex == 0)
+            {
+                price = 450;
+                shippingChargePerc = calculateQuantity(price, quantity)*shippingCharges;
+                quantity = Convert.ToDouble(Quantity.Text);
+                MessageBox.Show($"Price : {price}" +
+                                $"\nQuantity : {Quantity.Text}" +
+                                $"\nShipping Charges at % : {shippingChargePerc }" + 
+                                $"\nTotal : {shippingChargePerc + calculateQuantity(price, quantity)}");
+
+
+            }
+            else if (listBox1.SelectedIndex == 1)
+            {
+                price = 545;
+                shippingChargePerc = calculateQuantity(price, quantity) * shippingCharges;
+                quantity = Convert.ToDouble(Quantity.Text);
+                MessageBox.Show($"Price : {price}" +
+                                $"\nQuantity : {Quantity.Text}" +
+                                $"\nShipping Charges at % : {shippingChargePerc }" +
+                                $"\nTotal : {shippingChargePerc + calculateQuantity(price, quantity)}");
+
+            }
+            else if (listBox1.SelectedIndex == 2)
+            {
+                price = 240;
+                shippingChargePerc = calculateQuantity(price, quantity) * shippingCharges;
+                quantity = Convert.ToDouble(Quantity.Text);
+                MessageBox.Show($"Price : {price}" +
+                                $"\nQuantity : {Quantity.Text}" +
+                                $"\nShipping Charges at % : {shippingChargePerc }" +
+                                $"\nTotal : {shippingChargePerc + calculateQuantity(price, quantity)}");
+
+            }
+            else if (listBox1.SelectedIndex == 3)
+            {
+                price = 630;
+                shippingChargePerc = calculateQuantity(price, quantity) * shippingCharges;
+                quantity = Convert.ToDouble(Quantity.Text);
+                MessageBox.Show($"Price : {price}" +
+                                $"\nQuantity : {Quantity.Text}" +
+                                $"\nShipping Charges at % : {shippingChargePerc }" +
+                                $"\nTotal : {shippingChargePerc + calculateQuantity(price, quantity)}");
+
+            }
+            else if (listBox1.SelectedIndex == 4)
+            {
+                price = 120;
+                shippingChargePerc = calculateQuantity(price, quantity) * shippingCharges;
+                quantity = Convert.ToDouble(Quantity.Text);
+                MessageBox.Show($"Price : {price}" +
+                                $"\nQuantity : {Quantity.Text}" +
+                                $"\nShipping Charges at % : {shippingChargePerc }" +
+                                $"\nTotal : {shippingChargePerc + calculateQuantity(price, quantity)}");
+
+            }
+        }
+    }
+}
